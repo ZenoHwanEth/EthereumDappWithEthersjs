7ec7e8b8b46ef3489c12ac7310517ec6c3fa1f4b
HwanChinYang <hwan2221@bjak.my>
2022-02-12 20:26:45 +0800

    first commit

---
+++ sample-solidity/ERC-20_LW3.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract LW3Token is ERC20 {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {
+        _mint(msg.sender, 10 * 10**18);
+    }
+}
