4f0cecd8546c5d4003ab7f74c3139e32886d291f
HwanChinYang <hwan2221@bjak.my>
2022-03-26 23:32:48 +0800

    making ico

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x896cB6A4230cAe78F01bC16F908f45FaB8126467";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
