16d20aff6257557e7995d6cc3933f6ea7b501b33
HwanChinYang <hwan2221@bjak.my>
2022-03-26 21:19:45 +0800

    change metadata url

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xF42f940822778633502cAB82803Dbfd0100A7253";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL =
+  "https://nft-collection-go8bhr339-zenohwaneth.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
