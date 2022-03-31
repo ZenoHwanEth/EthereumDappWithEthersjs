f3eb4afd7549df043f6b45bd9ced15ac69669ff7
ZenoHwanEth <hwan2221@hotmail.com>
2022-04-01 01:08:19 +0800

    testing out hardhat verification

---
+++ hardhat-verification/contracts/Verify.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Verify {
+    string private greeting;
+
+    constructor() {}
+
+    function hello(bool sayHello) public pure returns (string memory) {
+        if (sayHello) {
+            return "hello";
+        }
+        return "";
+    }
+}
